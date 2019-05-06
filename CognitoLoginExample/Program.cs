46c0dc5f2c0bc14017737e1717b4b531b9a40607
Drew Cutchins <drew.cutch@gmail.com>
2019-05-06 15:02:12 -0700

    Create Solution

--- CognitoLoginExample/Program.cs
+++ CognitoLoginExample/Program.cs
@@ -3,19 +3,113 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Amazon;
+using Amazon.CognitoIdentityProvider;
+using Amazon.CognitoIdentityProvider.Model;
+using static CognitoLoginExample.ConsoleHelper;
 
 namespace CognitoLoginExample
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        
+        private static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            while (true)
+            {
+                int choice = ConsoleHelper.MultipleChoice("Select Action", "Create Account", "Login", "Quit");
+                Console.Clear();
+                switch (choice)
+                {
+                    case 0:
+                    {
+                        CreateAccount();
+                        break;
+                    }
+                    case 1:
+                    {
+                        Login();
+                        break;
+                    }
+                    case 2:
+                    {
+                        return;
+                    }
+                }
+            }
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+
+        }
+
+        private static void Login()
+        {
+            while(true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter your credentials");
+                    Console.Write("Username: ");
+                    string username = Console.ReadLine();
+                    Console.Write("Password: ");
+                    string password = Console.ReadLine();
+                    Task<string> signInTask = AccountUtils.GetCredsAsync(username, password);
+                    signInTask.Wait();
+                    string idToken = signInTask.Result;
+                    PrintSuccess("Log-in successful!");
+                    Console.WriteLine("idToken:");
+                    Console.WriteLine(idToken);
+                    break;
+                }
+                catch (AggregateException e)
+                {
+                    foreach (Exception innerException in e.InnerExceptions)
+                    {
+                        switch (innerException)
+                        {
+                            case UserNotFoundException _:
+                                PrintError("User does not exist!");
+                                break;
+                            case NotAuthorizedException _:
+                                PrintError("Incorrect password!");
+                                break;
+                            default:
+                                throw innerException;
+                        }
+                    }
+                    if(!ConsoleHelper.TryAgain())
+                        return;
+                }
+
+            }
+            Console.ReadLine();
+        }
+
+        private static void CreateAccount()
+        {
+            /*
+            Console.WriteLine("Enter Fields:");
+            string birthdate = Prompt("Birthdate");
+            string churchUsername = Prompt("Church Username");
+            string email = Prompt("Email");
+            string firstName = Prompt("First Name");
+            */
+            TogetherUser newUser = new TogetherUser
+            {
+                BirthDate = "1999-08-18",
+                ChurchUsername = "NPCChurch3",
+                Email = "drew.cutch@gmail.com",
+                FirstName = "Andrew",
+                LastName = "Cutchins",
+                Password = "Test1234",
+                Status = new UserStatusType("test"),
+                Username = "DrewCutch"
+            };
+            Task signUpTask = AccountUtils.CreateAsync(newUser);
+            signUpTask.Wait();
+            PrintSuccess("Account successfully created");
+            Console.ReadLine();
         }
     }
+
+    
 }
